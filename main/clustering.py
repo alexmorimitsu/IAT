2619f37ec26580d45849513bfce5afcc906570e6
AleM <pptalem@gmail.com>
2022-04-26 14:30:26 -0300

    Updating clustering.py to preserve global structure

--- main/clustering.py
+++ main/clustering.py
@@ -1,6 +1,4 @@
 import sys
-assert len(sys.argv) >= 2
-
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -11,8 +9,9 @@ from keras.models import Model
 import timeit
 import numpy as np
 import pandas as pd
-from sklearn.manifold import TSNE
-#from openTSNE import TSNE
+#from sklearn.manifold import TSNE
+from openTSNE import TSNE
+
 #from umap import UMAP
 import random
 import matplotlib.pyplot as plt 
@@ -22,7 +21,9 @@ import time
 
 #os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-def read_model(model_name = 'inception', model_path = 'models/inception/model/', base_model_path = 'models/inception/base_model/', layer_name = 'conv_7b_ac'):
+assert len(sys.argv) >= 3
+
+def read_model(model_name = 'inception', model_path = '../models/inception/model/', base_model_path = '../models/inception/base_model/'):
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     
@@ -89,6 +90,7 @@ def compute_features(layer_name, model, base_model, images):
     print(current_time, 'Computing features of ', layer_name, '...')
 
     final_model = get_final_model(model, base_model, layer_name)
+
     x = final_model.predict(images)
 
     image_names = []
@@ -119,40 +121,33 @@ def read_csv(filename, feature_names):
     
     return names, features
 
-def compute_pca(features, n=2):
-    '''
-        Input:
-            n: number of PCA dimensions
-        Output:
-            PCA with n dimensions
-    '''
-
-    pca = PCA(n_components=n)
-    return pca.fit_transform(features)
-
-def compute_tsne(features, n=2):            
+def compute_tsne(features, n=2, base_tsne = []):            
     #tsne = TSNE(n_components=n)
     #return tsne.fit_transform(features)
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     print(current_time, 't-SNE starting...')
 
-    #tsne = TSNE( #opentsne
-    #    n_components=n,
-    #    perplexity=30,
-    #    initialization="pca",
-    #    metric="cosine",
-    #    n_jobs=8,
-    #    random_state=3,
-    #).fit(features)
-    tsne = TSNE(n_components=n, method='exact', verbose=1).fit_transform(features)
+    if base_tsne == []:
+        tsne = TSNE(
+            n_components=n,
+            perplexity=30,
+            initialization="pca",
+            metric="cosine",
+            n_jobs=8,
+            random_state=3,
+        )
+    
+        tsne_results = tsne.fit(features)
+
+    else:
+        tsne_results = base_tsne.transform(features)
 
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     print(current_time, 't-SNE computed\n')
-    print(tsne.shape)
 
-    return tsne
+    return tsne_results
 
 #def compute_umap(features, n=2):            
 #    umap = UMAP(n_components=n)
@@ -160,7 +155,7 @@ def compute_tsne(features, n=2):
 
 def save_csv(features, features2d, image_names, csv_path, file_name = ''):
     header = ['names', 'x', 'y', 'custom_data', 'manual_label', 'correct_label',\
-    'x2', 'y2', 'colors', 'y3', 'x4', 'y4', 'D1','D2','D3','D4','D5','D6','D7']
+    'x2', 'y2', 'x3', 'y3', 'x4', 'colors', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7']
 
     basenames = [os.path.basename(name) for name in image_names]
 
@@ -184,12 +179,9 @@ def save_csv(features, features2d, image_names, csv_path, file_name = ''):
     correct_labels = np.array(correct_labels).reshape((len(image_names), 1))
     zeros_6cols = np.zeros((len(image_names), 6))
 
-    print(names_col.shape)
-    print(features2d.shape)
-    print(features.shape)
+    transposed_features = np.transpose(np.array(features))[0]
 
-    
-    data = np.hstack((names_col, features2d, ids, manual_labels, correct_labels, zeros_6cols, features))
+    data = np.hstack((names_col, features2d, ids, manual_labels, correct_labels, zeros_6cols, transposed_features))
     
     df = pd.DataFrame(data, columns = header)
     df.to_csv(csv_path + file_name + '.csv', index=False)
@@ -198,45 +190,61 @@ def save_csv(features, features2d, image_names, csv_path, file_name = ''):
     current_time = time.strftime("%H:%M:%S", t)
     print(current_time, csv_path + '.csv saved\n')
 
-def main(path_to_parent, csv_path):
-#    layer_names_1d = ['block17_1_ac', 'block17_6_ac', 'block17_11_ac', 'block17_16_ac', 'mixed_7a', 'block8_1_ac', 'conv_7b_ac']
-    layer_names_1d = ['mixed_7a']
-    layer_names_2d = ['mixed_7a']
+def generate_dataframe(csv_path, base_path):
+    layer_names_1d = ['block17_1_ac', 'block17_6_ac', 'block17_11_ac', 'block17_16_ac', 'mixed_7a', 'block8_1_ac', 'conv_7b_ac']
+    
+    layer_name_2d = 'mixed_7a'
 #    feature_names = ['Layer_A', 'Layer_B', 'Layer_C', 'Layer_D', 'Layer_E', 'Layer_F', 'Layer_G']
-    tsnes = []
     img_height, img_width, model, base_model = read_model()
-    images = prepare_images(path_to_parent, img_width, img_height)    
+    base_images = prepare_images(base_path, img_width, img_height)    
 
+    base_features, image_names = compute_features(layer_name_2d, model, base_model, base_images)
+    
+    base_tsne = compute_tsne(base_features, 2)
+    
+    tsnes = []
     for layer_name in layer_names_1d:
-        features, _ = compute_features(layer_name, model, base_model, images)
-        tsne = compute_tsne(features, 7)
-        #tsnes.append(tsne[0])
-    #transposed_features = np.transpose(np.array(tsnes))[0]
-    tsnes = tsne
+        features_1d, _ = compute_features(layer_name, model, base_model, base_images)
+        tsne_1d = compute_tsne(features_1d, 1)
+        tsnes.append(tsne_1d)
+        print(tsne_1d.shape)
+    
+    file_name = layer_name_2d + '.csv'
+    save_csv(tsnes, base_tsne, image_names, csv_path, file_name)
 
-    for layer_name in layer_names_2d:
-        features, image_names = compute_features(layer_name, model, base_model, images)
-        tsne2d = compute_tsne(features, 2)
-        file_name = ''
-        if len(layer_names_2d) > 1:
-            file_name = '_' + layer_name
-        save_csv(tsnes, tsne2d, image_names, csv_path, file_name)
+def generate_transformed_dataframe(csv_path, base_path, transformed_path):
+    layer_names_1d = ['block17_1_ac', 'block17_6_ac', 'block17_11_ac', 'block17_16_ac', 'mixed_7a', 'block8_1_ac', 'conv_7b_ac']
+    
+    layer_name_2d = 'mixed_7a'
+#    feature_names = ['Layer_A', 'Layer_B', 'Layer_C', 'Layer_D', 'Layer_E', 'Layer_F', 'Layer_G']
+    img_height, img_width, model, base_model = read_model()
+    base_images = prepare_images(base_path, img_width, img_height)    
+    transformed_images = prepare_images(transformed_path, img_width, img_height)    
 
-dataset_path = sys.argv[1]
-
-if len(sys.argv) >= 3:
-    csv_path = sys.argv[2]
-else:
-    csv_path = './projection'
+    base_features, _ = compute_features(layer_name_2d, model, base_model, base_images)
+    transformed_features, image_names = compute_features(layer_name_2d, model, base_model, transformed_images)
+    #np.savetxt(layer_name + '_base.csv', features, delimiter=',')
+    
+    base_tsne = compute_tsne(base_features, 2)
+    transformed_tsne = compute_tsne(transformed_features, 2, base_tsne)
+    
+    tsnes = []
+    for layer_name in layer_names_1d:
+        features_1d, _ = compute_features(layer_name, model, base_model, transformed_images)
+        #np.savetxt(layer_name + '_1d.csv', data, delimiter=',')
+        tsne_1d = compute_tsne(features_1d, 1)
+        tsnes.append(tsne_1d)
+        print(tsne_1d.shape)
+    
+    file_name = layer_name_2d + '.csv'
+    save_csv(tsnes, transformed_tsne, image_names, csv_path, file_name)
 
 if __name__ == '__main__':
-    main(dataset_path, csv_path)
-
-print('\nfeatures were extracted.')
-
-print('\n2D projection was generated.')
-
-print('\nnow proceed with the command:')
-
-print('\npython app.py assets/unlabeled/samples/ ' + csv_path + '.csv\n')
-
+    csv_path = sys.argv[1]
+    base_path = sys.argv[2]
+    
+    if len(sys.argv) == 3:
+        generate_dataframe(csv_path, base_path)
+    else:
+        transformed_path = sys.argv[3]
+        generate_transformed_dataframe(csv_path, base_path, transformed_path)
