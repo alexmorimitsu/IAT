137be6a98a7d1ca7c1a3d53c6fa73123b95a65ad
Alexandre Morimitsu <alexmorimitsu@gmail.com>
2023-04-10 13:53:53 -0300

    Fixing io_coscinodiscus runner

--- run_io_coscinodiscus.py
+++ run_io_coscinodiscus.py
@@ -35,7 +35,7 @@ def check_input(text, max_num):
 
 projects_path = 'main/assets/'
 
-project_name = 'io_consinodiscus'
+project_name = 'io_coscinodiscus'
 
 dataframes_path = 'main/assets/' + project_name + '/dataframes/'
 samples_path = 'main/assets/' + project_name + '/samples/' + project_name
